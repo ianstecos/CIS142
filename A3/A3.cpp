3425042c18d193c9bf454b2966d610f8f49402f0
Cosmin Iancu <ianstecos@gmail.com>
2022-09-16 18:55:07 -0500

    Submit Assignment #3

---
+++ A3/A3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Subject \t"
+          "Grade"
+       << endl
+       << "Math 098 \t"
+          "92"
+       << endl
+       << "English 100 \t"
+          "77"
+       << endl
+       << "CIS 142 \t"
+          "88";
+  return 0;
+}
