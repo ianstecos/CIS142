ea98e5a8c7e770958bca65aba62df9b9e9e47f90
Cosmin Iancu <ianstecos@gmail.com>
2022-09-16 20:02:37 -0500

    Submit Assignment #5

---
+++ A5/A5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // cout << "My precious!\n";
+  // cout << "My \n precious!\n";
+  // cout << "n\nn";
+  // cout << "n\\nn";
+  // cout << "There are 4 hobbits.\n";
+  // cout << "There are " << 4 << " hobbits.\n";
+  // cout << "There are" << 4 << "hobbits.\n";
+  // cout << "There are " << 2 + 2 << " hobbits.\n";
+  // cout << "There are " << "2+2" << " hobbits.\n";
+  // What statement would you write if you wanted to print the phrase "My
+  // precious!" to the screen, with the quotation marks?
+  // (cout << "\"My precious!\"\n";)
+  return 0;
+}
