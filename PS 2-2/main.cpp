413fbfd30e54e7fdf01009e05ac63086c179e896
Cosmin Iancu <ianstecos@gmail.com>
2022-09-26 20:14:08 -0500

    Programming Exercise 2-2

---
+++ PS 2-2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "CCCCCCCCC       ++              ++" << endl
+         << "CC              ++              ++" << endl
+         << "CC        ++++++++++++++  +++++++++++++++" << endl
+         << "CC        ++++++++++++++  +++++++++++++++" << endl
+         << "CC              ++              ++" << endl
+         << "CCCCCCCCC       ++              ++";
+    return 0;
+}
