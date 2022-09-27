962d3e3de2f48f59f4c325a4abf43427609b9050
Cosmin Iancu <ianstecos@gmail.com>
2022-09-26 19:57:30 -0500

    Programming Exercise 2-1

---
+++ PS 2-1/PS_2-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "**********************************" << endl
+         << "*    Programming Assignment 1    *" << endl
+         << "*     Computer Programming I     *" << endl
+         << "*             Author: ???        *" << endl // Replace ??? with your name and center it
+         << "*   Due Date: Thursday, Jan. 24  *" << endl
+         << "**********************************";
+    return 0;
+}
