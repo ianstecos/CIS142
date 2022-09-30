72b3c82c5e91717f1da141bd2d983eaf0102f357
Cosmin Iancu <ianstecos@gmail.com>
2022-09-29 22:06:32 -0500

    Programming Exercise 2-28

---
+++ MindTap/Unit 2/PS 2-28/main.cpp
@@ -0,0 +1,48 @@
+/*
+
+Summary
+
+In an elementary school, a mixture of equal amounts of nuts and dried fruit is
+provided during lunch. Suppose that the number of calories in each pound of
+nuts is 0.70 times the number of calories in each pound of dried fruit.
+
+Instructions
+
+Write a program that prompts the user to input:
+
+The number of students in the elementary school
+The number of calories required for each student from the mixture
+The number of calories in each pound of nuts.
+The program outputs:
+
+The amount of nuts and dried fruit needed for the students.
+For simplicity, assume that each student requires the same amount of calories.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int studentsNumber;
+  double caloriesPerStudent, nutsCalories, fruitCalories, amount;
+
+  cout << "Enter students number: ";
+  cin >> studentsNumber;
+
+  cout << "Enter calories required per student: ";
+  cin >> caloriesPerStudent;
+
+  cout << "Enter calories in each pound of nuts: ";
+  cin >> nutsCalories;
+
+  fruitCalories = nutsCalories / 0.7;
+  amount = (caloriesPerStudent / (nutsCalories + fruitCalories)) * studentsNumber;
+
+  cout << "Amount of nuts:" << amount << endl;
+  cout << "Amount of dried fruit:" << amount << endl;
+
+  return 0;
+}
