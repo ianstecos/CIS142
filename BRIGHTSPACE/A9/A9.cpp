3b1a2cf06836cac1139d598d7b14bd422287de31
Cosmin Iancu <ianstecos@gmail.com>
2022-10-16 20:21:01 +0000

    Submit Assignment 9

--- BRIGHTSPACE/A9/A9.cpp
+++ BRIGHTSPACE/A9/A9.cpp
@@ -1,28 +1,71 @@
+/*
+
+Develop a function that will accept a string and report how many vowels there
+are in the string. The function should return an integer and accept a character
+string. You are only writing a function; the function should not prompt the
+user for input.
+
+countVowels(“This is the time for all men”) => 8
+countVowels(“Golden Eye”) = > 4
+countVowels(“Monday”) => 2
+
+Usage:
+cout << “Number of vowels” << countVowels(“Mission”) << endl;
+
+or
+
+int numOfVowels = 0;
+numberOfVowels = countVowels(“Mission”)
+
+*/
+
 #include <iostream>
+#include <string>
 using namespace std;
 
-int cars(int, int, int);
+int countVowels(string text)
+{
+  int counter = 0;
 
-int main() {
-  cout << cars(88, 37, 12) << endl;
+  for (int i = 0; i <= text.length(); i++)
+  {
 
-  return 0;
+    switch (toupper(text[i]))
+    {
+    case 'A':
+      counter++;
+      break;
+    case 'E':
+      counter++;
+      break;
+    case 'I':
+      counter++;
+      break;
+    case 'O':
+      counter++;
+      break;
+    case 'U':
+      counter++;
+      break;
+    default:
+      break;
+    }
+  }
+
+  return counter;
 }
 
-int cars(int wheels, int carBodies, int figurines) {
-  int cars = 0;
+int main()
+{
+  string text;
+  int vowels = 0;
 
-    int wheelsSets = wheels / 4;
-    int carBodiesSets = carBodies / 1;
-    int figurinesSets = figurines / 2;
+  cout << "Enter text: ";
+  getline(cin, text);
 
-    if (wheelsSets <= carBodiesSets && wheelsSets <= figurinesSets ) {
-        cars = wheelsSets;   
-    } else if (carBodiesSets <= wheelsSets && carBodiesSets <= figurinesSets){
-        cars = carBodiesSets;
-    } else {
-        cars = figurinesSets;
-    } 
+  vowels = countVowels(text);
 
-  return cars;
+  cout << "Total vowels counted: " << vowels << endl;
+
+  return 0;
 }
---
+++ MINDTAP/Unit-4/4-5/main.cpp
@@ -0,0 +1,19 @@
+/*
+
+Programming Exercise 4-5
+Author: Cosmin Iancu
+
+Instructions
+In a right triangle, the square of the length of one side is equal to the sum 
+of the squares of the lengths of the other two sides.
+
+Write a program that prompts the user to enter the lengths of three sides of a 
+triangle and then outputs a message indicating whether the triangle is a right 
+triangle.
+
+If the triangle is a right triangle, output It is a right angled triangle
+
+If the triangle is not a right triangle, output It is not a right angled 
+triangle
+
+*/
