2a081493b014c6676cfcb5e35e7f2b3dfefe642b
Cosmin Iancu <ianstecos@gmail.com>
2022-10-22 05:27:52 +0000

    Programming Exercise 4-16

---
+++ MINDTAP/Unit-4/4-16/main.cpp
@@ -0,0 +1,35 @@
+/*
+
+Programming Exercise 4-16
+Author: Cosmin Iancu
+
+Summary
+A new author is in the process of negotiating a contract for a new romance 
+novel. The publisher is offering three options.
+
+- In the first option, the author is paid $5,000 upon delivery of the final 
+  manuscript and $20,000 when the novel is published.
+- In the second option, the author is paid 12.5% of the net price of the novel 
+  for each copy of the novel sold.
+- In the third option, the author is paid 10% of the net price for the first 
+  4,000 copies sold, and 14% of the net price for the copies sold over 4,000.
+
+The author has some idea about the number of copies that will be sold and would 
+like to have an estimate of the royalties generated under each option.
+
+Instructions
+Write a program that prompts the author to enter:
+
+1. The estimated number of copies that will be sold.
+2. The net price of each copy of the novel
+
+The program then outputs:
+
+1. The royalties under each option
+2. The best option the author could choose.
+   - Ex. If option 1 is the best, output Option 1 is the best
+
+(Use appropriate named constants to store the special values such as royalty 
+rates and fixed royalties.)
+
+*/
