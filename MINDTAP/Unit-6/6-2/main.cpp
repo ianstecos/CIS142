a4ac39ebc632350beb4ceb454fd0fe46a879d287
Cosmin Iancu <ianstecos@gmail.com>
2022-11-07 18:15:45 -0600

    fix: backend regexp bug with MindTap

--- MINDTAP/Unit-6/6-2/main.cpp
+++ MINDTAP/Unit-6/6-2/main.cpp
@@ -3,14 +3,14 @@ Programming Exercise 6-2
 Author: Cosmin Iancu
 
 Instructions
-Write a value-returning function, isVowel, that returns the value true if a 
-given character is a vowel and otherwise returns false.
+Write a value-returning function, isVowel, that returns the value true if a
+given caracter is a vowel and otherwise returns false.
 
 For the input E, your output should look like the following:
 
 E is a vowel: 1
 
-When printing the value of a bool, true will be displayed as 1 and false will 
+When printing the value of a bool, true will be displayed as 1 and false will
 be displayed as 0.
 */
 
@@ -18,19 +18,7 @@ be displayed as 0.
 
 using namespace std;
 
-bool isVowel(char character)
-{
-  char c = toupper(character);
-
-  if ((c == 'A') || (c == 'E') || (c == 'I') || (c == 'O') || (c == 'U'))
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
-}
+bool isVowel(char c);
 
 int main()
 {
@@ -38,4 +26,17 @@ int main()
   cin >> c;
   cout << c << " is a vowel: " << isVowel(c) << endl;
   return 0;
+}
+
+bool isVowel(char c)
+{
+  if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+      c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
