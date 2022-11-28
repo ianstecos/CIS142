1969a5e006908943011b3082d5406259d63c4007
Cosmin Iancu <ianstecos@gmail.com>
2022-11-27 21:47:28 -0600

    Programming Exercise 9-3

---
+++ Unit-9/9-3/main.cpp
@@ -0,0 +1,22 @@
+/*
+Programming Exercise 9-3
+Author: Cosmin Iancu
+
+Instructions
+Define a struct menuItemType with two components: menuItem of type string and 
+menuPrice of type double.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  struct menuItemType
+  {
+    string menuItem;
+    double menuPrice;
+  };
+  return 0;
+}
